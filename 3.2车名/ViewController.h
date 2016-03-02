e5b96c999b3f14ab3f72e5b380a166d2595b5f16
邓金明 <Jimmy@dengjinmingdemacbook-pro.local>
2016-03-02 13:14:15 +0800

    Initial Commit

---
+++ 3.2车名/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3.2车名
+//
+//  Created by 邓金明 on 16/3/2.
+//  Copyright © 2016年 邓金明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 3.2车名Tests/__2__Tests.m
@@ -0,0 +1,39 @@
+//
+//  __2__Tests.m
+//  3.2车名Tests
+//
+//  Created by 邓金明 on 16/3/2.
+//  Copyright © 2016年 邓金明. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __2__Tests : XCTestCase
+
+@end
+
+@implementation __2__Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
